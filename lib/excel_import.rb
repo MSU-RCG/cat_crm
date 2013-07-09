90ca0ac76fd029bfd5e1288588059b8ab17928b3
Sean Cleveland <sean.b.cleveland@gmail.com>
2013-07-09 14:37:00 -0600

    updated importer for new excel form

--- lib/excel_import.rb
+++ lib/excel_import.rb
@@ -5,31 +5,25 @@ require 'spreadsheet'
 require 'fileutils'
 
   def self.parse(filename)
-	ex = Roo::Excelx.new(filename) 
-	ex.default_sheet = ex.sheets.first
-	header = ex.first_row
-	footer = ex.last_row
-	
-	2.upto(footer) {|line|
-		l = Lead.new()
-		a = Address.create()
-		l.first_name = ex.cell(line, 'A')
-		l.last_name = ex.cell(line, 'B')
-		l.email = ex.cell(line, 'C')
-		l.phone = ex.cell(line, 'D')
-		l.source = ex.cell(line, 'E').downcase
-		l.status = ex.cell(line, 'F').downcase
-		l.cf_academic_year = ex.cell(line, 'G').to_s
-		a.street1 = ex.cell(line, 'H').to_s
-		a.street2 = ex.cell(line, 'I').to_s
-		a.city = ex.cell(line, 'J').to_s
-		a.state = ex.cell(line, 'K').to_s
-		a.zipcode = ex.cell(line, 'L').to_i.to_s
-		a.country = ex.cell(line, 'M').to_s
-		a.address_type = "Business"
-		a.save
-		l.business_address = a
-		l.save
-	}
+    ex = Roo::Excelx.new(filename) 
+    ex.default_sheet = ex.sheets.first
+    header = ex.first_row
+    footer = ex.last_row
+
+    2.upto(footer) {|line|
+      l = Lead.where(:last_name=>ex.cell(line, 'B').to_s, 
+                     :first_name=>ex.cell(line, 'C').to_s
+                     ).first_or_create
+      a = Address.create(:street1=>ex.cell(line, 'E').to_s, :city=> ex.cell(line, 'F').to_s, :state=>ex.cell(line, 'G').to_s, :zipcode=>ex.cell(line, 'H').to_i.to_s,:address_type => "Business")
+      email = ex.cell(line, 'K').to_s
+      l.phone = ex.cell(line, 'I').to_s + ex.cell(line, 'J').to_s
+      l.source = ex.cell(line, 'M').to_s.downcase
+      year = ex.cell(line, 'L').to_s.strip[0..3]
+      end_year = year.to_i + 1
+      l.cf_academic_year = year + "-" + end_year.to_s
+      l.cf_starting_year = year + "-" + end_year.to_s
+      l.business_address = a
+      l.save
+    }
   end
 end
