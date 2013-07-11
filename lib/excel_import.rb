e498b6546422011758d057b4b4ebd7c532dffed1
Sean Cleveland <sean.b.cleveland@gmail.com>
2013-07-11 15:46:36 -0600

    more error catching

--- lib/excel_import.rb
+++ lib/excel_import.rb
@@ -15,14 +15,34 @@ require 'fileutils'
                      :first_name=>ex.cell(line, 'C').to_s
                      ).first_or_create
       a = Address.create(:street1=>ex.cell(line, 'E').to_s, :city=> ex.cell(line, 'F').to_s, :state=>ex.cell(line, 'G').to_s, :zipcode=>ex.cell(line, 'H').to_i.to_s,:address_type => "Business")
-      Lead.update(l.id, :email => ex.cell(line, 'K').to_s)
-      Lead.update(l.id, :phone => ex.cell(line, 'I').to_s + ex.cell(line, 'J').to_s)
-      Lead.update(l.id, :source => ex.cell(line, 'M').to_s.downcase.gsub(' ','_'))
-      year = ex.cell(line, 'L').to_s.strip[0..3]
-      end_year = year.to_i + 1
-      Lead.update(l.id, :cf_academic_year => year + "-" + end_year.to_s)
+      begin
+        Lead.update(l.id, :email => ex.cell(line, 'K').to_s)
+      rescue Exception => e
+        puts "email ERROR" + e.message
+      end
+      begin
+        Lead.update(l.id, :phone => ex.cell(line, 'I').to_s + ex.cell(line, 'J').to_s)
+      rescue Exception => e
+       puts "phone ERROR" + e.message
+      end
+      begin
+        Lead.update(l.id, :source => ex.cell(line, 'M').to_s.downcase.gsub(' ','_'))
+      rescue Exception => e
+       puts "source ERROR" + e.message
+      end
+      begin
+        year = ex.cell(line, 'L').to_s.strip[0..3]
+        end_year = year.to_i + 1
+        Lead.update(l.id, :cf_academic_year => year + "-" + end_year.to_s)
+      rescue Exception => e
+        puts "cf_academic_year ERROR" + e.message
+      end
       # l.cf_starting_year = year + "-" + end_year.to_s
+      begin
       Lead.update(l.id, :business_address => a)
+      rescue Exception => e
+       puts "business_address ERROR" + e.message
+      end
       #l.save(:validate=>false)
     }
   end
