e5c5735ff0d14b633ff8cd2a1ae0bfe34ac92729
Sean Cleveland <sean.b.cleveland@gmail.com>
2013-07-11 15:24:57 -0600

    update excel import

--- lib/excel_import.rb
+++ lib/excel_import.rb
@@ -15,15 +15,15 @@ require 'fileutils'
                      :first_name=>ex.cell(line, 'C').to_s
                      ).first_or_create
       a = Address.create(:street1=>ex.cell(line, 'E').to_s, :city=> ex.cell(line, 'F').to_s, :state=>ex.cell(line, 'G').to_s, :zipcode=>ex.cell(line, 'H').to_i.to_s,:address_type => "Business")
-      l.email = ex.cell(line, 'K').to_s
-      l.phone = ex.cell(line, 'I').to_s + ex.cell(line, 'J').to_s
-      l.source = ex.cell(line, 'M').to_s.downcase.gsub(' ','_')
+      Lead.update(l.id, :email => ex.cell(line, 'K').to_s)
+      Lead.update(l.id, :phone => ex.cell(line, 'I').to_s + ex.cell(line, 'J').to_s)
+      Lead.update(l.id, :source => ex.cell(line, 'M').to_s.downcase.gsub(' ','_'))
       year = ex.cell(line, 'L').to_s.strip[0..3]
       end_year = year.to_i + 1
-      l.cf_academic_year = year + "-" + end_year.to_s
+      Lead.update(l.id, :cf_academic_year => year + "-" + end_year.to_s)
       # l.cf_starting_year = year + "-" + end_year.to_s
-      l.business_address = a
-      l.save(:validate=>false)
+      Lead.update(l.id, :business_address => a)
+      #l.save(:validate=>false)
     }
   end
 end
